6e83d9873282eca1769a9f8ffafc9b9514d3c428
menachem95 <menachemf95@gmail.com>
2022-12-26 14:14:52 +0200

    .

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   useEffect(() => {
     let responseData = printers;
     const fetchPrinters = async () => {
-      const response = await fetch(`${process.env.SERVER}/fetch-printers`, {
+      const response = await fetch(process.env.SERVER_FETCH, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       });
@@ -47,7 +47,7 @@ function App() {
       responseData = await response.json();
 
 
-      const res = await fetch(`${process.env.SERVER}/ping`, {
+      const res = await fetch(process.env.SERVER_PING, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(
