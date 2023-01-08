bd9cfffc3477bdbe49e661a027f63dfbe8ec36bb
menachem95 <menachemf95@gmail.com>
2023-01-08 11:36:28 +0200

    new_link

--- src/App.js
+++ src/App.js
@@ -44,8 +44,8 @@ function App() {
     let responseData = printers;
     const fetchPrinters = async () => {
       const response = await fetch(
-        // "https://hospitol-demo-server.onrender.com/fetch-printers"
-        "http://localhost:8080/fetch-printers",
+        "https://hospitol-demo-server.onrender.com/fetch-printers"
+        // "http://localhost:8080/fetch-printers",
         {
           method: "GET",
           headers: { "Content-Type": "application/json" },
