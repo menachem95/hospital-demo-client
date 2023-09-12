3f8a74c975ebc5b23c3c936fefd7d464c7426d29
menachem95 <menachemf95@gmail.com>
2023-09-12 15:55:48 +0300

    .

--- src/components/Displays/SearchResult.js
+++ src/components/Displays/SearchResult.js
@@ -26,78 +26,7 @@ const SearchResult = () => {
 
   if (searchKey.trim() === "") return <Dashboard />
 
-  let department = printers.filter(
-    (printer) => printer.department === departmentId
-  ); //[]
-
-  // if (deviceId === "printers"){
-  //   department = printers.filter(
-  //   (printer) => printer.department === departmentId
-  // );
-  // } else if (deviceId === "computers"){
-  //   department = computers.filter(
-  //     (computer) => computer.department === departmentId
-  //   );
-  // }
-
-  const onlineDevices = department.filter((device) => device.online).length;
-
-  //   if (deviceId === "computers"){
-  //     return (
-  //       <Box m="20px">
-  //         <Box
-  //           display="flex"
-  //           justifyContent="space-between"
-  //           alignItems="center"
-  //           position="sticky"
-  //           top="80px"
-  //         >
-  //           <Header
-  //             title={departmentId}
-  //             subtitle={`מחשבים מקוונים: ${onlineDevices}/${department.length}`}
-  //           />
-  //         </Box>
-  //         <Box
-  //           display="grid"
-  //           gridTemplateColumns="repeat(12, 1fr)"
-  //           gridAutoRows="140px"
-  //           gap="20px"
-  //         >
-  //           {department.map((printer) => {
-  //             return (
-  //               <Box
-  //                 key={printer.pag}
-  //                 gridColumn="span 3"
-  //                 gridRow="span 2"
-  //                 backgroundColor={colors.primary[400]}
-  //                 display="flex"
-  //                 alignItems="center"
-  //                 justifyContent="center"
-  //               ><Printer
-  //               room={printer.room}
-  //               address={printer.address}
-  //               online={printer.online}
-  //               pag={printer.pag}
-  //               line={printer.line}
-  //               department={printer.department}
-  //               description={printer.description}
-  //               model={printer.model}
-  //               icon={
-  //                 <ComputerIcon
-  //                   sx={{
-  //                     color: colors.greenAccent[600],
-  //                     fontSize: "90px",
-  //                   }}
-  //                 />
-  //               }
-  //             />
-
-  //       </Box>
-  //     );
-  //   })}
-  // </Box>
-  // </Box>
-  //  ) }
+  
 
   const keys = ["address", "pag", "printerModel", "department", "description", "room"];
 
@@ -125,8 +54,8 @@ const SearchResult = () => {
           top="80px"
         >
           <Header
-            title={departmentId}
-            subtitle={`מדפסות מקוונות: ${onlineDevices}/${department.length}`}
+            // title={"תוצאות חיפוש"}
+            title={`נמצאו: ${filtersPrinters.length} מדפסות`}
           />
         </Box>
         <Box
